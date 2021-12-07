ef6cda05d992e9f619d49eb5dc351bf8949dbed0
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2021-12-07 22:26:33 +0530

    Product screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
