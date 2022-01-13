8db4be6aa195ecec452ea44b20c180a0d2029e00
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-13 22:15:09 +0530

    Order controller and routes code added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,14 +85,16 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
 
   if (user) {
-    user.name = req.body.name || user.name
-    user.email = req.body.email || user.email
+    console.log(req.body);
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
     if (req.body.password) {
-      user.password = req.body.password
+      user.password = req.body.password;
     }
 
-    const updatedUser = await user.save()
-
+    const updatedUser = await user.save();//pre-save hook hashes the updated password before storing it in db
+    
+    //console.log(updatedUser, "updated user test");
     res.json({
       _id: updatedUser._id,
       name: updatedUser.name,
@@ -101,9 +103,9 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       token: generateToken(updatedUser._id),
     })
   } else {
-    res.status(404)
-    throw new Error('User not found')
+    res.status(404);
+    throw new Error('User not found');
   }
 })
 
-export { authUser, registerUser, getUserProfile, updateUserProfile }
+export { authUser, registerUser, getUserProfile, updateUserProfile };
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import orderRoutes from './routes/orderRoutes.js'
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound);
 app.use(errorHandler);
