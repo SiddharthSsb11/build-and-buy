f4e65c574e63e82a2395b9cb88c9348d5256ce3a
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-14 18:37:00 +0530

    routes and controller created for getting order details

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes)
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
