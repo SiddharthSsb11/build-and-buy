59afaed24ed088f34c57bfdcd0226bd57d342548
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-15 23:47:04 +0530

    deployment test

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
@@ -15,15 +16,27 @@ connectDB();//mongodb connection
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.send('API is runnnnnning...')
-});
+}); */
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  })
+}
 
 app.use(notFound);
 app.use(errorHandler);
