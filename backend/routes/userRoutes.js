c1d9489bdca5f024eb93558a2f230c9fd80aa7df
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-15 06:51:25 +0530

    admin MW, contorllers and routes added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser);
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
