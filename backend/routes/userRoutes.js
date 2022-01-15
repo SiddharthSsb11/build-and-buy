8a93e67d0e3af12a0984b9f68053217e58fdbdaa
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-01-15 16:01:18 +0530

    admin deleting users routes&controller added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const router = express.Router();
-import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } from '../controllers/userController.js';
+import { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser } from '../controllers/userController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser).get(protect, admin, getUsers);
+
 router.post('/login', authUser);
 
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+
+router.route('/:id').delete(protect, admin, deleteUser);
 
 export default router;
