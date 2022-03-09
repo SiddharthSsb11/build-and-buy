9268a8782607094c29dc3861b334cb47267df7ba
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-03-09 22:38:00 +0530

    Guest user login added

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,15 +1,15 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
-import { login } from '../actions/userActions'
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -19,61 +19,77 @@ const LoginScreen = () => {
   //console.log(userLogin, 'userlogin state');
   const { loading, error, userInfo } = userLogin;
 
-  const redirect = location.search ? location.search.split('=')[1] : '/';
+  const redirect = location.search ? location.search.split("=")[1] : "/";
 
   useEffect(() => {
-    if (userInfo) {//if (!(userInfo === undefined || userInfo.length === 0))
+    if (userInfo) {
+      //if (!(userInfo === undefined || userInfo.length === 0))
       navigate(redirect);
     }
-  }, [navigate, userInfo, redirect])
+  }, [navigate, userInfo, redirect]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     dispatch(login(email, password));
-  }
+  };
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
 
-      {error && <Message variant='danger'>{error}</Message>}
+      {error && <Message variant="danger">{error}</Message>}
       {loading && <Loader />}
-      
-      <Form onSubmit={submitHandler}>
 
-        <Form.Group controlId='email'>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type='email'
-            placeholder='Enter email'
+            type="email"
+            placeholder="Enter email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='password'>
+        <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Enter password'
+            type="password"
+            placeholder="Enter password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Button className="mt-3" type='submit' variant='primary'> Sign In </Button>
+        <Button
+          className="mt-3"
+          type="submit"
+          variant="primary"
+          style={{ marginRight: "1rem" }}
+        >
+          {" "}
+          Sign In{" "}
+        </Button>
+        <Button className="mt-3" type="button" variant="secondary" 
+          onClick={() => {
+            setEmail("sid@test.com");
+            setPassword("sidtest");
+          }}>
+          {" "}
+          Guest User Sign In{" "}
+        </Button>
       </Form>
 
-      <Row className='py-3'>
+      <Row className="py-3">
         <Col>
-          New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+          New Customer?{" "}
+          <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
             Register
           </Link>
         </Col>
       </Row>
     </FormContainer>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { PayPalButton } from 'react-paypal-button-v2';
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import {  Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message';
-import Loader from '../components/Loader';
-import { getOrderDetails, payOrder, deliverOrder } from '../actions/orderActions';
-import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from '../constants/orderConstants';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { PayPalButton } from "react-paypal-button-v2";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import {
+  getOrderDetails,
+  payOrder,
+  deliverOrder,
+} from "../actions/orderActions";
+import {
+  ORDER_PAY_RESET,
+  ORDER_DELIVER_RESET,
+} from "../constants/orderConstants";
 
 const OrderScreen = () => {
-  
   const [sdkReady, setSdkReady] = useState(false);
 
-  const {id} = useParams();  
+  const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -31,93 +37,91 @@ const OrderScreen = () => {
 
   if (!loading) {
     //Total order amount //not sved in db(not included in og schema)//recalculating here
-    order.itemsPrice = order.orderItems.reduce( (acc, item) => acc + item.price * item.qty, 0 );
+    order.itemsPrice = order.orderItems.reduce(
+      (acc, item) => acc + item.price * item.qty,
+      0
+    );
   }
 
   useEffect(() => {
-
-    if(!userInfo){
-      navigate('/login');
+    if (!userInfo) {
+      navigate("/login");
     }
 
     const addPayPalScript = async () => {
-      const { data: clientId } = await axios.get('/api/config/paypal');
-      const script = document.createElement('script');
-      script.type = 'text/javascript';
+      const { data: clientId } = await axios.get("/api/config/paypal");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
       //https://www.paypal.com/sdk/js?client-id=${clientId}
       //https://www.paypal.com/sdk/js?client-id=${clientId}&currency=INR
       script.async = true;
       script.onload = () => {
         setSdkReady(true);
-      }
+      };
       document.body.appendChild(script);
-    }
+    };
 
-    if(!order || order._id !== id || successPay || successDeliver) {//(!order)
+    if (!order || order._id !== id || successPay || successDeliver) {
+      //(!order)
       dispatch({ type: ORDER_PAY_RESET }); //prevents infinite loop //successPay value changes
       dispatch({ type: ORDER_DELIVER_RESET });
       dispatch(getOrderDetails(id));
-
     } else if (!order.isPaid) {
-
       if (!window.paypal) {
         addPayPalScript();
-
       } else {
         setSdkReady(true);
-      } 
-     /*  if (!window.paypal) {
+      }
+      /*  if (!window.paypal) {
         addPayPalScript()
 
       } */
-
     }
-    
-  }, [dispatch, id, order, successPay, successDeliver, navigate, userInfo])
+  }, [dispatch, id, order, successPay, successDeliver, navigate, userInfo]);
 
   const successPaymentHandler = (paymentResult) => {
     //console.log(paymentResult,'payment result from paypal');
     dispatch(payOrder(id, paymentResult));
-  }
+  };
 
   const deliverHandler = () => {
     dispatch(deliverOrder(order));
-  }
+  };
 
   ///{<PayPalButton currency="INR" amount={order.totalPrice} onSuccess={successPaymentHandler} />}
 
   return loading ? (
     <Loader />
   ) : error ? (
-    <Message variant='danger'>{error}</Message>
+    <Message variant="danger">{error}</Message>
   ) : (
     <>
       <h1>Order ID: {order._id}</h1>
       <Row>
         <Col md={8}>
-          <ListGroup variant='flush'>
+          <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
                 <strong>Name: </strong> {order.user.name}
               </p>
               <p>
-                <strong>Email: </strong>{' '}
+                <strong>Email: </strong>{" "}
                 <a href={`mailto:${order.user.email}`}>{order.user.email}</a>
               </p>
               <p>
                 <strong>Address: </strong>
-                {order.shippingAddress.address}, {order.shippingAddress.city}{' '}
-                {order.shippingAddress.postalCode},{' '}
+                {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+                {order.shippingAddress.postalCode},{" "}
                 {order.shippingAddress.country}
               </p>
               {order.isDelivered ? (
-                <Message variant='success'>
+                <Message variant="success">
                   Delivered on {order.deliveredAt.substring(0, 10)}
                 </Message>
               ) : (
-                <Message variant='danger'>Not Delivered</Message>
+                <Message variant="danger">Not Delivered</Message>
               )}
             </ListGroup.Item>
 
@@ -128,9 +132,11 @@ const OrderScreen = () => {
                 {order.paymentMethod}
               </p>
               {order.isPaid ? (
-                <Message variant='success'>Paid on {order.paidAt.substring(0, 10)}</Message>
+                <Message variant="success">
+                  Paid on {order.paidAt.substring(0, 10)}
+                </Message>
               ) : (
-                <Message variant='danger'>Not Paid</Message>
+                <Message variant="danger">Not Paid</Message>
               )}
             </ListGroup.Item>
 
@@ -139,7 +145,7 @@ const OrderScreen = () => {
               {order.orderItems.length === 0 ? (
                 <Message>Order is empty</Message>
               ) : (
-                <ListGroup variant='flush'>
+                <ListGroup variant="flush">
                   {order.orderItems.map((item, index) => (
                     <ListGroup.Item key={item.product}>
                       <Row>
@@ -157,7 +163,8 @@ const OrderScreen = () => {
                           </Link>
                         </Col>
                         <Col md={4}>
-                          {item.qty} x ₹ {item.price} = ₹ {item.qty * item.price}
+                          {item.qty} x ₹ {item.price} = ₹{" "}
+                          {item.qty * item.price}
                         </Col>
                       </Row>
                     </ListGroup.Item>
@@ -169,7 +176,7 @@ const OrderScreen = () => {
         </Col>
         <Col md={4}>
           <Card>
-            <ListGroup variant='flush'>
+            <ListGroup variant="flush">
               <ListGroup.Item>
                 <h2>Order Summary</h2>
               </ListGroup.Item>
@@ -198,34 +205,59 @@ const OrderScreen = () => {
                 </Row>
               </ListGroup.Item>
               {!order.isPaid && (
-                <ListGroup.Item>
+                <ListGroup.Item style={{paddingTop:"1rem"}}>
                   {loadingPay && <Loader />}
                   {!sdkReady ? (
                     <Loader />
                   ) : (
-                    <PayPalButton amount={order.totalPrice} onSuccess={successPaymentHandler} />
+                    <PayPalButton
+                      amount={order.totalPrice}
+                      onSuccess={successPaymentHandler}
+                    />
+                  
                   )}
                 </ListGroup.Item>
               )}
               {loadingDeliver && <Loader />}
-              {userInfo && userInfo.isAdmin && order.isPaid && !order.isDelivered && (
-                <ListGroup.Item>
-                  <Button
-                    type='button'
-                    className='btn btn-block'
-                    onClick={deliverHandler}
-                  >
-                    Mark As Delivered
-                  </Button>
-                </ListGroup.Item>
-              )}
+              {userInfo &&
+                userInfo.isAdmin &&
+                order.isPaid &&
+                !order.isDelivered && (
+                  <ListGroup.Item>
+                    <Button
+                      type="button"
+                      className="btn btn-block"
+                      onClick={deliverHandler}
+                    >
+                      Mark As Delivered
+                    </Button>
+                  </ListGroup.Item>
+                )}
+              
+              {!order.isPaid && (
+                <ListGroup.Item style={{ color: "gray", fontWeight: "normal", marginTop:"-6px"}}>
+                <p >
+                  ** PayPal Sandbox dummy generated payment details
+                </p>
+                <table style={{fontSize: "13px", marginTop:"-8px"}}>
+                  <tr>
+                    <td>** Email: sb-tregl11668856@personal.example.com</td>
+                  </tr>
+                  <tr>
+                    <td>** Password: Y%7XuL+g</td>
+
+                  </tr>
+                  
+                </table>
+              </ListGroup.Item>
+              )}  
+              
             </ListGroup>
           </Card>
         </Col>
       </Row>
     </>
-  )
-}
+  );
+};
 
-
-export default OrderScreen
+export default OrderScreen;
