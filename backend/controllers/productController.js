8acff01df4894ea96d1939c5bd99d3b9c1a5704d
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-05-07 23:57:49 +0530

    search by name and brand code logic added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,38 @@
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 //import Order from '../models/orderModel.js';
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-
-  const keyword = req.query.keyword
-  ? {
+  /* const keyword = req.query.keyword
+  ///////? {
       name: {
         $regex: req.query.keyword,
         $options: 'i',
       },
     }
-  : {}
+  : {} */
+  // Keyword searches by Name and Brand
+  const keyword = req.query.keyword
+    ? {
+        $or: [
+          {
+            name: {
+              $regex: req.query.keyword,
+              $options: "i",
+            },
+          },
+          {
+            brand: {
+              $regex: req.query.keyword,
+              $options: "i",
+            },
+          },
+        ],
+      }
+    : {};
   //console.log(keyword);
 
   //const products = await Product.find({});
@@ -23,11 +41,11 @@ const getProducts = asyncHandler(async (req, res) => {
   const page = Number(req.query.pageNumber) || 1;
 
   const count = await Product.count(); */
-  const products = await Product.find({...keyword}) //.limit(pageSize).skip(pageSize * (page - 1));
+  const products = await Product.find({ ...keyword }); //.limit(pageSize).skip(pageSize * (page - 1));
   res.json(products);
 
   //res.json({ products, page, pages: Math.ceil(count / pageSize) });
-})
+});
 
 // @desc    Fetch single product
 // @route   GET /api/products/:id
@@ -39,10 +57,9 @@ const getProductById = asyncHandler(async (req, res) => {
     res.json(product);
   } else {
     res.status(404);
-    throw new Error('Product not found');
+    throw new Error("Product not found");
   }
-})
-
+});
 
 // @desc    Register a new user
 // @route   POST /api/users
@@ -81,16 +98,18 @@ const getProductById = asyncHandler(async (req, res) => {
 // @route   POST /api/products/:id/reviews
 // @access  Private
 const createProductReview = asyncHandler(async (req, res) => {
-  const { rating, comment } = req.body
+  const { rating, comment } = req.body;
 
-  const product = await Product.findById(req.params.id)
+  const product = await Product.findById(req.params.id);
 
   if (product) {
-    const alreadyReviewed = product.reviews.find( (r) => r.user.toString() === req.user._id.toString() )
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
 
     if (alreadyReviewed) {
       res.status(400);
-      throw new Error('Product already reviewed');
+      throw new Error("Product already reviewed");
     }
 
     const review = {
@@ -98,32 +117,30 @@ const createProductReview = asyncHandler(async (req, res) => {
       rating: Number(rating),
       comment,
       user: req.user._id,
-    }
+    };
 
     product.reviews.push(review);
 
     product.numReviews = product.reviews.length;
 
-    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
 
     await product.save();
-    res.status(201).json({ message: 'Review added' });
+    res.status(201).json({ message: "Review added" });
   } else {
     res.status(404);
-    throw new Error('Product not found');
+    throw new Error("Product not found");
   }
-})
-
+});
 
 // @desc    Get top rated products
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => {
-
   const products = await Product.find({}).sort({ rating: -1 }).limit(3);
   res.json(products);
-
-})
-
+});
 
 export { getProducts, getProductById, createProductReview, getTopProducts };
