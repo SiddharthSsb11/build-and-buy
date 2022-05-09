5737fdf0d266c731a77b1b6bcf19e4113746d5b1
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-05-09 23:29:14 +0530

    deployment ready

--- frontend/src/components/Filter.js
+++ frontend/src/components/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button, ButtonGroup } from "react-bootstrap";
 
 const Filter = ({products, setFilteredProducts}) => {
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col, Accordion, Form, Button } from "react-bootstrap";
+import { Row, Col, Accordion } from "react-bootstrap";
 import Product from "../components/Product";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
